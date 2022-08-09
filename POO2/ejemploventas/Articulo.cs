32c753a49291a533b396cfeab09032fefa004437
tomasoyarzabal <tomasoyarzabal135@gmail.com>
2022-08-09 17:40:13 -0300

    POO2

---
+++ POO2/ejemploventas/Articulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemploventas
+{
+      class Articulo
+    {
+        public int CodArticulo { get; set; }
+        public float Precio { get; set; }
+
+        private int codMarca;
+
+        public int CodigoMarca
+        {
+            get { return codMarca; }
+            set
+            {
+                if (value > 0 && value < 11)
+
+                    codMarca = value;
+
+                else codMarca = -1;
+            }
+        }
+    }
+}
---
+++ POO2/ejemploventas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemploventas
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Articulo[] articulo = new Articulo[10];
+
+            for (int x = 0; x < 10; x++)
+            {
+                articulo[x] = new Articulo();
+                Console.WriteLine("ingrese los datos del producto");
+                Console.WriteLine("codigo");
+                articulo[x].CodArticulo = int.Parse(Console.ReadLine());
+                Console.WriteLine("ingrese el precio");
+                articulo[x].Precio = float.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese el codigo de marca");
+                articulo[x].CodigoMarca = int.Parse(Console.ReadLine());
+
+            }
+        }
+    }
+}
